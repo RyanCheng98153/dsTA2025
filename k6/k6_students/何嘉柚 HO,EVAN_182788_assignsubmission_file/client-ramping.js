d4b4ba322f1e2b83cf3f3be85dd18175065713d9
ryan98153 <110703007@nccu.edu.tw>
2025-06-04 12:08:35 +0800

    chore: k6_students moodle zip file

---
+++ k6/k6_students/何嘉柚 HO,EVAN_182788_assignsubmission_file/client-ramping.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '2s', target: 400 },
+    { duration: '2s', target: 800 },
+    { duration: '2s', target: 1200 },
+    { duration: '2s', target: 1600 },
+    { duration: '2s', target: 2000 },
+    { duration: '2s', target: 2400 },
+  ],
+};
+
+export default function () {
+  http.post('http://localhost:3000/shop');
+  sleep(1);
+}
