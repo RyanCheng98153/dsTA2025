4bb6c902daf6e2e7555b8a59852b638b977d7ae7
ryan98153 <110703007@nccu.edu.tw>
2025-06-04 12:09:34 +0800

    feat: k6 check homework initial file

---
+++ k6/parse_hw.py
@@ -0,0 +1,47 @@
+import os
+import csv
+
+hw_dir = "./mqtt_students"
+output_path = "hw_mqtt.csv"
+target_file = "game.js"
+
+with open(output_path, mode='w', newline='') as file:
+    # writer = csv.writer(file)
+    pass
+
+for stud_dir in os.listdir(hw_dir):
+    stud_name = stud_dir.split(' ')[0]
+    # print(stud_dir)
+    
+    if not os.path.isdir(os.path.join(hw_dir, stud_dir)):
+        continue
+    
+    stud_dir = hw_dir + "/" + stud_dir
+    
+    if stud_dir + target_file in os.listdir(stud_dir):
+        continue
+    
+    need_move = False
+    
+    # for directory in stud_dir, if server.js is in the directory, then print it
+    for directory in os.listdir(stud_dir):
+        filename = os.path.join(stud_dir, directory, target_file)
+        dirname = os.path.join(stud_dir, directory)
+        if os.path.isfile(filename):
+            need_move = True
+            # print(f"Found {target_file} in {directory} for student {stud_name}")
+        break
+    
+    if not need_move:
+        continue
+    
+    if os.path.exists(dirname):
+        print(f"Directory {dirname} exists.")
+        
+        # move the files in directory to the parent directory
+        for filename in os.listdir(dirname):
+            src = os.path.join(dirname, filename)
+            dst = os.path.join(stud_dir, filename)
+            os.rename(src, dst)
+            print(f"Moved {src} to {dst}")
+        
